5202c8787a58891517982d5bb93e8a48b18f4ba2
Elgussgood <gsuarezr@alumno.ipn.mx>
2022-09-23 20:49:08 -0500

    Ver4

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app=express();
 //le decimos al servidor que utilizamos public para archivos del cliente 
 app.use(express.static('public'))
 app.get('/ruta-get',(req,res)=>{
-    res.send('<h1> Hola mundo desde la ruta get')
+    res.send('<h1> Hola mundo desde la ruta get</h1>')
 })
 
 app.post('/ruta-post',(req,res)=>{
-    res.send('<h1> Hola mundo desde la ruta post')
+    res.send('<h1> Hola mundo desde la ruta post</h1>')
 })
 app.listen(8080,()=>{
-    console.log("Puerto escuchando en 3000")
+    console.log("Puerto escuchando en 8080")
 })
